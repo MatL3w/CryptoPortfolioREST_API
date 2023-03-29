925d2bf8fba7005e1484a13f8bc44dd6a6da2ba3
Lecislav <lecislav@protonmail.com>
2023-03-29 10:58:29 +0200

    json parsing and response for request

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+//packages
 import express from "express";
+import bodyParser from 'body-parser';
+
+//project modules
 import feedRouter from './routes/feed.js';
 
+
+//core
 const app = express();
 
+app.use(bodyParser.json());
 app.use(feedRouter.router);
 
 app.listen(3000);
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,8 @@
 const helloWorld= (req,res, next)=>{
     console.log("hello world");
+    res.status(200).json({
+        hello:'world'
+    })
 }
 
 export default {helloWorld};
