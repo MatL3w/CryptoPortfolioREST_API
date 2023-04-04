5a70254f073e87af21890f8e68b4491a3632b68d
Lecislav <lecislav@protonmail.com>
2023-04-04 13:07:30 +0200

    resolving potential CORS problem

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ import * as config from './config.js';
 const app = express();
 
 app.use(bodyParser.json());
-
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
+  res.setHeader("Access-Control-Allow-Headers","X-Requested-With,content-type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(authRouter.router);
 app.use(feedRouter.router);
 
