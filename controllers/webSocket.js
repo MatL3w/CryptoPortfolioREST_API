70221b095b63e5527b0b1d17c5b816b3cb49f1c4
Lecislav <lecislav@protonmail.com>
2023-04-05 11:57:32 +0200

    *

--- controllers/webSocket.js
+++ controllers/webSocket.js
@@ -5,23 +5,17 @@ import * as util from "../util/util.js"
 expressWs(express());
 
 export const socketConnection = (ws, req) => {
-    console.log(ws.userId);
     ws.interval  = setInterval(async function () {
         const defilamaStatus = await util.defilamaIsOnline();
         const message = {
             online: defilamaStatus,
         };
-        console.log(message);
-        console.log(ws.userId);
         ws.send(JSON.stringify(message));
     }, 5000);
     ws.on("message", (msg) => {
-        console.log(msg);
         ws.send(msg);
     });
     ws.on("close", function close() {
         clearInterval(ws.interval);
-      console.log("disconnected");
     });
-
 };
