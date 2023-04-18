bc2928fc069c7d7a6772be17ba48796f970402f5
Lecislav <lecislav@protonmail.com>
2023-04-18 21:59:15 +0200

    refactoring Endpoints

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ else {
     app.use(function (req, res, next) {
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
-        res.setHeader("Access-Control-Allow-Headers","X-Requested-With,content-type");
+        res.setHeader("Access-Control-Allow-Headers","*");
         res.setHeader("Access-Control-Allow-Credentials", true);
         next();
     });
--- routes/feed.js
+++ routes/feed.js
@@ -6,7 +6,7 @@ import * as Authentication from "../middleware/is-auth.js";
 
 export const router = Router();
 
-router.post("/upsertasset",
+router.post("/asset",
   header("Authorization").not().isEmpty().isJWT(),
   body("assetNameTag").not().isEmpty().isString().trim(),
   body("assetQuantity").toFloat().isNumeric(),
@@ -14,22 +14,15 @@ router.post("/upsertasset",
   feedController.upsertAsset
 );
 
-router.delete("/deleteasset",
+router.delete("/asset",
     Authentication.isAuth,
     header("Authorization").not().isEmpty().isJWT(),
     body("assetNameTag").not().isEmpty().isString().trim(),
     feedController.deleteAsset
 );
 
-router.get("/getassets",
+router.get("/getasset",
     header("Authorization").not().isEmpty().isJWT(),
     Authentication.isAuth,
     feedController.getAssets
-);
-
-router.get( "/getasset",
-    header("Authorization").not().isEmpty().isJWT(),
-    body("assetNameTag").not().isEmpty().isString().trim(),
-    Authentication.isAuth,
-    feedController.getAsset
 );
